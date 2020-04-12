9f9d9685d30ea1dea27ffccce5aa0c2768ccc41d
Ugo Loobuyck <ugo813@hotmail.fr>
2020-04-12 20:34:25 +0200

    Function transfert over, preproc test over

--- Pipeline/PPP.py
+++ Pipeline/PPP.py
@@ -22,13 +22,24 @@ from sklearn.model_selection import StratifiedShuffleSplit, train_test_split
 from .contraction import contraction_mapping
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+""" Globals """
 
-PORT = 9000
-CORENLP = True
+# Stanford NLP pipeline access
+nlp_tagger = StanfordCoreNLP('http://localhost:' + 9000)
+
+# VADER sentiment analysis tool
+analyzer = SentimentIntensityAnalyzer()
+
+# Expand word contractions or not
 EXPANSION = True
+
 FILEDIR = './files'
 if FILEDIR not in sys.path:
     sys.path.insert(1, FILEDIR)
+
+
+
+""" Wrapper functions """
 
 def file_split(data, tag_column='tag', to_csv=False, out_dir=None):
     """ Wrapper """
@@ -52,16 +63,37 @@ def preprocess_train(data, text_column='text_review', tag_column='tag', split_va
 
 def preprocess_test(data, text_column='text_review', tag_column='tag', tag_pattern='abcd'):
     data = _early_check(data, tag_column)
-    _to_numeric(data, )
+    data = data.loc[:, [text_column, tag_column]]
+    _to_numeric(data, tag_column, tag_pattern)
+    _html_url_cleaning(data, text_column)
+    _flesch_ease(data, text_column)
+    _sentiment(data, text_column, analyzer)
+    _tokenize(data, text_column)
+    _to_lower(data, text_column)
+    _expand(data, text_column)
+    _join_split(data, text_column)
+    _remove_punct(data, text_column)
+    _num_to_words(data, text_column)
+    _join_split(data, text_column)
+    _length_features(data, text_column, char_level=True)
+    _join_words(data, text_column)
+    _annotate(data, text_column, nlp_tagger)
 
-def _early_check(data, tag_column):
+    return data
+
+
+""" Helper functions """
+
+def _early_check(data, text_column, tag_column):
     assert isinstance(data, pd.DataFrame), 'Wrong type!'
+    assert text_column in data.columns, "column {} was not found in DataFrame.".format(text_column)
+    assert tag_column in data.columns, "column {} was not found in DataFrame.".format(tag_column)
+
     if 'Unnamed: 0' in data.columns:
         return data.drop('Unnamed: 0', axis=1)
     data.dropna(subset=[tag_column], inplace=True)
     data.reset_index(drop=True, inplace=True)
     return data
-
 
 def _tr_te_split(data, tag_col='tag', te_size=0.1):
     assert tag_col in data.columns, "No column {} found in DataFrame.".format(tag_col)
@@ -79,26 +111,68 @@ def _to_numeric(data, tag_column, pattern='abcd'):
         data['bin_tag'] = data.loc[:, tag_column].map({0: 0, 1: 0, 2: 1, 3: 1})
     elif pattern == 'yesno':
         data[tag_column] = data.loc[:, tag_column].map({'no': 0, 'yes': 1})
-    
 
-class PPPipeline:
-    """ Preprocessing pipeline. Uses a DataHolder
-    object to perform series of operations on it. """
+def _html_url_cleaning(data, text_column):
+        data[text_column] = [re.sub(r'(<.*?>)|((\[\[).*(\]\]))|(\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*)', '', html.unescape(x)) for x in data.loc[:, text_column]]
 
-    def __init__(self, holder, corenlp=True, port=9000, expansion=True):
+def _tokenize(data, text_column):
+        if type(data[text_column][0]) is list:
+            data[text_column] = data.loc[:, text_column].map(lambda x: casual_tokenize(' '.join(x)))
+        else:
+            data[text_column] = data.loc[:, text_column].map(lambda x: casual_tokenize(x))
+
+def _split_join(data, text_column):
+    data[text_column] = data.loc[:, text_column].map(lambda x: ' '.join(x.split()))
+
+def _join_split(data, text_column):
+    data[text_column] = data.loc[:, text_column].map(lambda x: (' '.join(x)).split())
+
+def _join_words(data, text_column):
+    data[text_column] = [' '.join(x) for x in data.loc[:, text_column]]
+
+def _to_lower(data, text_column):
+    data[text_column] = [[x.lower() for x in i] for i in data[text_column]]
+
+def _expand(data, text_column):
+    data[text_column] = data.loc[:, text_column].map(lambda x: [contraction_mapping[i] if i in contraction_mapping else i for i in x])
+
+def _remove_punct(data, text_column):
+    data[text_column] = [[x for x in i if x not in set(string.punctuation)] for i in data[text_column]]
+
+def _num_to_words(data, text_column):
+    data[text_column] = [[num2words(x) if x.isdigit() else x for x in i] for i in data[text_column]]
+
+def _annotate(data, text_column, tagger):
+    assert isinstance(tagger, StanfordCoreNLP)
+    tags, lemmas = [], []
+    for i in data[text_column]:
+        annot = tagger.annotate(i,
+            properties={
+                'annotators': 'pos,lemma',
+                'outputFormat': 'json',
+                'timeout': 10000,
+            })
         
-        assert isinstance(holder, DataHolder), "Argument 'holder' must be a DataHolder object."
-        self.holder = holder
-        self.corenlp = corenlp
-        self.port = port
-        self.expansion = expansion
-        self.analyzer = SentimentIntensityAnalyzer()
-        
-        if self.corenlp:
-            print("Make sure that the CoreNLP server is up and running.\n"
-            "This scripts listens to localhost:9000")
-            self.nlp_tagger = StanfordCoreNLP('http://localhost:' + str(self.port))
+        tags.append(' '.join([x['pos'] for x in annot['sentences'][0]['tokens']]))
+        lemmas.append(' '.join([x['lemma'] for x in annot['sentences'][0]['tokens']]))
 
+    data['text_pos'] = tags
+    data['lemmas'] = lemmas
+
+def _sentiment(data, text_column, anal):
+    data['sentiment'] = [anal.polarity_scores(x)['compound'] for x in data[text_column]]
+
+def _flesch_ease(data, text_column):
+    data['read_score'] = [textstat.flesch_reading_ease(x) for x in data.loc[:, text_column]]
+
+def _length_features(data, text_column, char_level=True):
+    data['num_tokens'] = [len(x) for x in data.loc[:, text_column]]
+    if char_level:
+        data['num_char'] = [sum([len(x) for x in i]) for i in data.loc[:, text_column]]
+
+
+
+class PPPipeline:   
     def process_train_data(self, data):
         self.dropna('tag', self.df)
         self.to_numeric('tag', scheme='abcd')
